d64413c689f851fc03aef3891ba1b08264792cd9
diegoEmanuel77 <diegoo7u7.97@gmail.com>
2025-07-07 21:40:17 -0400

    actualizacion

--- resources/views/layouts/argon.blade.php
+++ resources/views/layouts/argon.blade.php
@@ -238,7 +238,25 @@
                     </ol>
                     
                 </nav>
-
+                <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+                        <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+                            
+                        </div>
+                        <ul class="navbar-nav  justify-content-end">
+                            
+                            <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+                                <a href="javascript:;" class="nav-link text-white p-0" id="iconNavbarSidenav">
+                                    <div class="sidenav-toggler-inner">
+                                        <i class="sidenav-toggler-line bg-white"></i>
+                                        <i class="sidenav-toggler-line bg-white"></i>
+                                        <i class="sidenav-toggler-line bg-white"></i>
+                                    </div>
+                                </a>
+                            </li>
+                           
+                        
+                        </ul>
+                </div>
             </div>
         </nav>
         <!-- End Navbar -->
