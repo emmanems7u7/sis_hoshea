73a0d069189ed5cb24c054d186a57bef85dc516c
diegoEmanuel77 <diegoo7u7.97@gmail.com>
2025-07-20 19:10:09 -0400

    cambios y ajustes

--- resources/views/tratamientos/firma.blade.php
+++ resources/views/tratamientos/firma.blade.php
@@ -1,5 +1,14 @@
-<footer style="font-size: 10px; text-align: center; position: absolute; bottom: 80; left: 0; right: 0;">
-    ___________________________<br>
-    <strong>{{ $user->nombre_completo }}</strong><br>
-    Firma del profesional
-</footer>
+@php
+
+    use App\Models\Configuracion;
+
+
+@endphp
+
+@if(Configuracion::first()->firma)
+    <footer style="font-size: 10px; text-align: center; position: absolute; bottom: 80; left: 0; right: 0;">
+        ___________________________<br>
+        <strong>{{ $user->nombre_completo }}</strong><br>
+        Firma del profesional
+    </footer>
+@endif
